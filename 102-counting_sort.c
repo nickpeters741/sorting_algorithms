27967894bd559c492a9e0be2772fd37936c7062b
nickpeters741 <npeters741@gmail.com>
2021-06-16 20:30:06 +0000

    Update

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,17 +10,21 @@
 void counting_sort(int *array, size_t size)
 {
 	size_t i;
-	int *count, *output, k, j, t = 0;
+	int *count, *output, k, j; /* t = 0;*/
 
 	if (size < 2)
 		return;
 
+/*
+ * Find the largest element in the array
+ */
 	k = 0;
 	for (i = 0; i < size; i++)
 	{
 		if (k < array[i])
 			k = array[i];
 	}
+
 	count = malloc(sizeof(int) * (k + 1));
 	output = malloc(sizeof(int) * size);
 	if (count == NULL || output == NULL)
@@ -28,18 +32,15 @@ void counting_sort(int *array, size_t size)
 	for (j = 0; j <= k; j++)
 		count[j] = 0;
 	for (i = 0; i < size; i++)
-		count[array[i]] = count[array[i]] + 1;
+		count[array[i]]++;
 	for (j = 1; j <= k; j++)
-	{
-		i = count[j];
-		count[j] = t;
-		t += i;
-/*		count[j] += count[j - 1];*/
-	}
+		count[j] += count[j - 1];
+
 	print_array(count, k + 1);
+
 	for (j = size - 1; j >= 0; j--)
 	{
-		output[count[array[j]]] = array[j];
+		output[count[array[j]] - 1] = array[j];
 		count[array[j]]--;
 	}
 	for (i = 0; i < size; i++)
